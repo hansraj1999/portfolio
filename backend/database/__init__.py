45df0a834b3b01c9c66f3ebc71cfeca9cb17cd27
Hansraj Deghun <deghun@gmail.com>
2025-11-16 13:14:13 +0530

    bug fixed for not exposing get projects func

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -2,13 +2,15 @@ from .mongodb import (
     connect_to_mongodb,
     close_mongodb_connection,
     get_database,
-    get_blogs_collection
+    get_blogs_collection,
+    get_projects_collection
 )
 
 __all__ = [
     'connect_to_mongodb',
     'close_mongodb_connection',
     'get_database',
-    'get_blogs_collection'
+    'get_blogs_collection',
+    'get_projects_collection'
 ]
 
